69978ebe900cd71397d4abc69e37d0c97a57d02d
putrapetirr0 <142271470+putrapetirr0@users.noreply.github.com>
2024-02-14 07:12:30 +0700

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -272,51 +272,52 @@ netfilter-persistent reload
 cd /usr/bin
 # menu
 wget -O menu "https://raw.githubusercontent.com/putrapetirr0/ARAZ/main/menu/menu.sh"
-wget -O menu-vmess "https://raw.githubusercontent.com/bracoli/v4/main/menu/menu-vmess.sh"
-wget -O menu-vless "https://raw.githubusercontent.com/bracoli/v4/main/menu/menu-vless.sh"
-wget -O running "https://raw.githubusercontent.com/bracoli/v4/main/menu/running.sh"
-wget -O clearcache "https://raw.githubusercontent.com/bracoli/v4/main/menu/clearcache.sh"
-wget -O menu-trgo "https://raw.githubusercontent.com/bracoli/v4/main/menu/menu-trgo.sh"
-wget -O menu-trojan "https://raw.githubusercontent.com/bracoli/v4/main/menu/menu-trojan.sh"
+wget -O menu-vmess "https://raw.githubusercontent.com/putrapetirr0/ARAZ/main/menu/menu-vmess.sh"
+wget -O menu-vless "https://raw.githubusercontent.com/putrapetirr0/ARAZ/main/menu/menu-vless.sh"
+wget -O running "https://raw.githubusercontent.com/putrapetirr0/ARAZ/main/menu/running.sh"
+wget -O clearcache "https://raw.githubusercontent.com/putrapetirr0/ARAZ/main/menu/clearcache.sh"
+wget -O menu-trgo "https://raw.githubusercontent.com/putrapetirr0/ARAZ/main/menu/menu-trgo.sh"
+wget -O menu-trojan "https://raw.githubusercontent.com/putrapetirr0/ARAZ/main/menu/menu-trojan.sh"
 
 # menu ssh ovpn
-wget -O menu-ssh "https://raw.githubusercontent.com/bracoli/v4/main/menu/menu-ssh.sh"
-wget -O usernew "https://raw.githubusercontent.com/bracoli/v4/main/ssh/usernew.sh"
-wget -O trial "https://raw.githubusercontent.com/bracoli/v4/main/ssh/trial.sh"
-wget -O renew "https://raw.githubusercontent.com/bracoli/v4/main/ssh/renew.sh"
-wget -O hapus "https://raw.githubusercontent.com/bracoli/v4/main/ssh/hapus.sh"
-wget -O cek "https://raw.githubusercontent.com/bracoli/v4/main/ssh/cek.sh"
-wget -O member "https://raw.githubusercontent.com/bracoli/v4/main/ssh/member.sh"
-wget -O delete "https://raw.githubusercontent.com/bracoli/v4/main/ssh/delete.sh"
-wget -O autokill "https://raw.githubusercontent.com/bracoli/v4/main/ssh/autokill.sh"
-wget -O ceklim "https://raw.githubusercontent.com/bracoli/v4/main/ssh/ceklim.sh"
-wget -O tendang "https://raw.githubusercontent.com/bracoli/v4/main/ssh/tendang.sh"
+wget -O menu-ssh "https://raw.githubusercontent.com/putrapetirr0/ARAZ/main/menu/menu-ssh.sh"
+wget -O usernew "https://raw.githubusercontent.com/putrapetirr0/ARAZ/main/ssh/usernew.sh"
+wget -O trial "https://raw.githubusercontent.com/putrapetirr0/ARAZ/main/ssh/trial.sh"
+wget -O renew "https://raw.githubusercontent.com/putrapetirr0/ARAZ/main/ssh/renew.sh"
+wget -O hapus "https://raw.githubusercontent.com/putrapetirr0/ARAZ/main/ssh/hapus.sh"
+wget -O cek "https://raw.githubusercontent.com/putrapetirr0/ARAZ/main/ssh/cek.sh"
+wget -O member "https://raw.githubusercontent.com/putrapetirr0/ARAZ/main/ssh/member.sh"
+wget -O delete "https://raw.githubusercontent.com/putrapetirr0/ARAZ/main/ssh/delete.sh"
+wget -O autokill "https://raw.githubusercontent.com/putrapetirr0/ARAZ/main/ssh/autokill.sh"
+wget -O ceklim "https://raw.githubusercontent.com/putrapetirr0/ARAZ/main/ssh/ceklim.sh"
+wget -O tendang "https://raw.githubusercontent.com/putrapetirr0/ARAZ/main/ssh/tendang.sh"
 
 # menu system
 wget -O menu-set "https://raw.githubusercontent.com/putrapetirr0/ARAZ/main/menu/menu-set.sh"
-wget -O menu-domain "https://raw.githubusercontent.com/bracoli/v4/main/menu/menu-domain.sh"
-wget -O add-host "https://raw.githubusercontent.com/bracoli/v4/main/ssh/add-host.sh"
-wget -O port-change "https://raw.githubusercontent.com/bracoli/v4/main/port/port-change.sh"
-wget -O certv2ray "https://raw.githubusercontent.com/bracoli/v4/main/xray/certv2ray.sh"
-wget -O menu-webmin "https://raw.githubusercontent.com/bracoli/v4/main/menu/menu-webmin.sh"
-wget -O speedtest "https://raw.githubusercontent.com/bracoli/v4/main/ssh/speedtest_cli.py"
+wget -O menu-domain "https://raw.githubusercontent.com/putrapetirr0/ARAZ/main/menu/menu-domain.sh"
+wget -O add-host "https://raw.githubusercontent.com/putrapetirr0/ARAZ/main/ssh/add-host.sh"
+wget -O port-change "https://raw.githubusercontent.com/putrapetirr0/ARAZ/main/port/port-change.sh"
+wget -O certv2ray "https://raw.githubusercontent.com/putrapetirr0/ARAZ/main/xray/certv2ray.sh"
+wget -O menu-webmin "https://raw.githubusercontent.com/putrapetirr0/ARAZ/main/menu/menu-webmin.sh"
+wget -O speedtest "https://raw.githubusercontent.com/putrapetirr0/ARAZ/main/ssh/speedtest_cli.py"
 wget -O about "https://raw.githubusercontent.com/putrapetirr0/ARAZ/main/menu/about.sh"
-wget -O auto-reboot "https://raw.githubusercontent.com/bracoli/v4/main/menu/auto-reboot.sh"
-wget -O restart "https://raw.githubusercontent.com/bracoli/v4/main/menu/restart.sh"
-wget -O bw "https://raw.githubusercontent.com/bracoli/v4/main/menu/bw.sh"
+wget -O auto-reboot "https://raw.githubusercontent.com/putrapetirr0/ARAZ/main/menu/auto-reboot.sh"
+wget -O restart "https://raw.githubusercontent.com/putrapetirr0/ARAZ/main/menu/restart.sh"
+wget -O bw "https://raw.githubusercontent.com/putrapetirr0/ARAZ/main/menu/bw.sh"
 wget -O auto backup "https://raw.githubusercontent.com/putrapetirr0/ARAZ/main/backup/autobackup.sh"
 wget -O backup "https://raw.githubusercontent.com/putrapetirr0/ARAZ/main/backup/backup.sh"
 wget -O restore "https://raw.githubusercontent.com/putrapetirr0/ARAZ/main/backup/restore.sh"
+wget -O backup-telegram "https://raw.githubusercontent.com/putrapetirr0/ARAZ/main/backup/backup-telegram.sh"
 
 # change port
-wget -O port-ssl "https://raw.githubusercontent.com/bracoli/v4/main/port/port-ssl.sh"
-wget -O port-ovpn "https://raw.githubusercontent.com/bracoli/v4/main/port/port-ovpn.sh"
+wget -O port-ssl "https://raw.githubusercontent.com/putrapetirr0/ARAZ/main/port/port-ssl.sh"
+wget -O port-ovpn "https://raw.githubusercontent.com/putrapetirr0/ARAZ/main/port/port-ovpn.sh"
 
 
-wget -O xp "https://raw.githubusercontent.com/bracoli/v4/main/ssh/xp.sh"
-wget -O acs-set "https://raw.githubusercontent.com/bracoli/v4/main/acs-set.sh"
+wget -O xp "https://raw.githubusercontent.com/putrapetirr0/ARAZ/main/ssh/xp.sh"
+wget -O acs-set "https://raw.githubusercontent.com/putrapetirr0/ARAZ/main/acs-set.sh"
 
-wget -O sshws "https://raw.githubusercontent.com/bracoli/v4/main/ssh/sshws.sh"
+wget -O sshws "https://raw.githubusercontent.com/putrapetirr0/ARAZ/main/ssh/sshws.sh"
 
 chmod +x menu
 chmod +x menu-vmess
@@ -352,6 +353,7 @@ chmod +x bw
 chmod +x auto backup
 chmod +x backup
 chmod +x restore
+chmod +x backup-telegram
 
 chmod +x port-ssl
 chmod +x port-ovpn
