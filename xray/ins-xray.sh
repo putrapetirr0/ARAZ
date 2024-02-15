676867db5c11d37920992067df4a773a86c225a4
putrapetirr0 <142271470+putrapetirr0@users.noreply.github.com>
2024-02-16 05:35:49 +0700

    Add files via upload

--- xray/ins-xray.sh
+++ xray/ins-xray.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-MYIP=$(wget -qO- ipinfo.io/ip);
-clear
-domain=$(cat /etc/xray/domain)
+echo -e "
+"
+date
+echo ""
+domain=$(cat /root/domain)
 sleep 1
-mkdir -p /etc/xray
+mkdir -p /etc/xray 
 echo -e "[ ${green}INFO${NC} ] Checking... "
 apt install iptables iptables-persistent -y
 sleep 1
@@ -38,10 +40,8 @@ sleep 1
 echo -e "[ ${green}INFO$NC ] Downloading & Installing xray core"
 domainSock_dir="/run/xray";! [ -d $domainSock_dir ] && mkdir  $domainSock_dir
 chown www-data.www-data $domainSock_dir
-
 # Make Folder XRay
 mkdir -p /var/log/xray
-mkdir -p /usr/bin/xray
 mkdir -p /etc/xray
 chown www-data.www-data /var/log/xray
 chmod +x /var/log/xray
@@ -49,19 +49,10 @@ touch /var/log/xray/access.log
 touch /var/log/xray/error.log
 touch /var/log/xray/access2.log
 touch /var/log/xray/error2.log
-
 # / / Ambil Xray Core Version Terbaru
-latest_version="$(curl -s https://api.github.com/repos/XTLS/Xray-core/releases | grep tag_name | sed -E 's/.*"v(.*)".*/\1/' | head -n 1)"
+bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install -u www-data --version 1.5.6
 
-# / / Installation Xray Core
-xraycore_link="https://github.com/XTLS/Xray-core/releases/download/v$latest_version/xray-linux-64.zip"
 
-# / / Unzip Xray Linux 64
-cd `mktemp -d`
-curl -sL "$xraycore_link" -o xray.zip
-unzip -q xray.zip && rm -rf xray.zip
-mv xray /usr/local/bin/xray
-chmod +x /usr/local/bin/xray
 
 ## crt xray
 systemctl stop nginx
@@ -71,17 +62,7 @@ chmod +x /root/.acme.sh/acme.sh
 /root/.acme.sh/acme.sh --upgrade --auto-upgrade
 /root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
 /root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
-/root/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key --ecc
-
-## crt nginx
-cd
-mkdir /etc/nginx/key.pem
-mkdir /etc/nginx/cert.pem
-cd
-acme.sh --install-cert -d $domain 
---key-file       /etc/nginx/key.pem  
---fullchain-file /etc/nginx/cert.pem 
---reloadcmd     "service nginx force-reload"
+~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key --ecc
 
 # nginx renew ssl
 echo -n '#!/bin/bash
@@ -470,8 +451,8 @@ END
 # Installing Trojan Go Service
 cat > /etc/systemd/system/trojan-go.service << END
 [Unit]
-Description=Trojan-Go Service Mod By ARAZ1308
-Documentation=github.com/putrapetirr0
+Description=Trojan-Go Service Mod By ADAM ARAZ
+Documentation=github.com/adammoi/vipies
 After=network.target nss-lookup.target
 
 [Service]
