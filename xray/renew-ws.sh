50601f6e916ebd061c99fab11e9c3818cd7df563
putrapetirr0 <142271470+putrapetirr0@users.noreply.github.com>
2024-02-17 20:43:39 +0700

    renew-ws.sh

--- xray/renew-ws.sh
+++ xray/renew-ws.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # My Telegram : https://t.me/araz1308
-# ==========================================
+# =========================================
 # Color
 RED='\033[0;31m'
 NC='\033[0m'
@@ -10,7 +10,7 @@ BLUE='\033[0;34m'
 PURPLE='\033[0;35m'
 CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
-# ==========================================
+# =========================================
 # Getting
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
