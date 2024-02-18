4f7902b74af030a119d697fa2a8b7d5e9cb89c1c
putrapetirr0 <142271470+putrapetirr0@users.noreply.github.com>
2024-02-18 16:55:35 +0700

    send.sh

--- backup/send.sh
+++ backup/send.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
+# Get date and time
 
 IP=$(wget -qO- ipinfo.io/ip);
 DATE=$(date +"%m-%d-%y")
 domain=$(cat /etc/xray/domain)
 ######################### Colours ############################
 ON_BLUE=$(echo -e "\033[44m")
+RED=$(echo -e "\033[1;31m")
+BLUE=$(echo -e "\033[1;34m")
+GREEN=$(echo -e "\033[1;32m")
 STD=$(echo -e "\033[0m") # Clear colour
+##############################################################
+echo "_____________________________________"
+echo " "
+echo "${GREEN} Title ${STD}"
+echo " "
+echo " ${RED} Message${STD}"
+echo " "
+echo "${GREEN} DATE:$DATE ${STD}"
+echo "_____________________________________"
+echo " "
 ######################## BOT INFO ############################
 BOT_TOKEN=$(cat /home/bot)
 CHAT_ID=$(cat /home/chat)
