2c7a56c22df22b59ee33c228a100b0a8f4e9edb3
putrapetirr0 <142271470+putrapetirr0@users.noreply.github.com>
2024-03-20 22:51:44 +0000

    trialtrojan.sh

--- xray/trialtrojan.sh
+++ xray/trialtrojan.sh
@@ -6,12 +6,38 @@ masaaktif=1
 exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
 sed -i '/#trojanws$/a\#! '"$user $exp"'\
 },{"password": "'""$uuid""'","email": "'""$user""'"' /etc/xray/config.json
-sed -i '/#trojangrpc$/a\#! '"$user $exp"'\
-},{"password": "'""$uuid""'","email": "'""$user""'"' /etc/xray/config.json
 
-systemctl restart xray
-trojanlink1="trojan://${uuid}@${domain}:${tr}?mode=gun&security=tls&type=grpc&serviceName=trojan-grpc&sni=bug.com#${user}"
-trojanlink="trojan://${uuid}@isi_bug_disini:${tr}?path=%2Ftrojan-ws&security=tls&host=${domain}&type=ws&sni=${domain}#${user}"
+trojanlink="trojan://${uuid}@${domain}:${tr}?path=%2Ftrojan-ws&security=tls&host=${domain}&type=ws&sni=${domain}#${user}"
+  ######################## send det ############################
+BOT_TOKEN="$(sed '/^$/d' /home/botdet)"
+CHAT_ID="$(sed '/^$/d' /home/chatdet)"
+file_path=""
+# Function to send a message to Telegram
+send_message() {
+  local message="$1"
+ curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" \
+ -d "chat_id=$CHAT_ID" \
+ -d "text=$message"
+}
+send_message "
+     Detail Account 
+━━━━━━━━━━━━━━━━━━━━━━━━━
+USERNAME  : $user
+
+DOMAIN    : $domain
+
+ID        : $uuid
+
+EXPIRED   : $exp
+
+LINK TLS  : ${trojanlink}
+"
+echo " "
+echo "_____________________________________"
+echo " "
+echo " ${RED} Message Sent ${STD}"
+echo "_____________________________________"
+echo " "
 clear
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "\E[0;41;36m           Trial TROJAN           \E[0m"
@@ -21,11 +47,8 @@ echo -e "Host/IP      : ${domain}"
 echo -e "port         : ${tr}"
 echo -e "Key          : ${uuid}"
 echo -e "Path         : /trojan-ws"
-echo -e "ServiceName  : trojan-grpc"
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "Link WS      : ${trojanlink}"
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "Link GRPC    : ${trojanlink1}"
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "Expired On   : $exp"
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
