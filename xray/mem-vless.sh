fcc5e0b4c1f92b6a369ba1308ec55c6af2cf2051
putrapetirr0 <142271470+putrapetirr0@users.noreply.github.com>
2024-05-14 23:53:48 +0100

    mem-vless.sh

--- xray/mem-vless.sh
+++ xray/mem-vless.sh
@@ -65,7 +65,7 @@ echo -e "port none TLS  : 80" | tee -a /etc/log-create-user.log
 echo -e "Id             : ${uuid}" | tee -a /etc/log-create-user.log
 echo -e "Encryption     : none" | tee -a /etc/log-create-user.log
 echo -e "Network        : ws" | tee -a /etc/log-create-user.log
-echo -e "Path           : /vless" | tee -a /etc/log-create-user.log
+echo -e "Path           : /vlessws" | tee -a /etc/log-create-user.log
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
 echo -e "Link TLS       : ${vlesslink1}" | tee -a /etc/log-create-user.log
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
