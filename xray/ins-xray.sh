b5d693760065c251493011c0d2220cd815a2c901
putrapetirr0 <142271470+putrapetirr0@users.noreply.github.com>
2024-05-14 23:49:14 +0100

    ins-xray.sh

--- xray/ins-xray.sh
+++ xray/ins-xray.sh
@@ -370,28 +370,22 @@ WantedBy=multi-user.target
 EOF
 
 #nginx config
-cat >/etc/nginx/conf.d/vlnone.conf <<EOF
-server {
-  listen 8880 backlog=65535 reuseport;
-  server_name $domain;
-  real_ip_header proxy_protocol;
-  real_ip_recursive on;
+cat >/etc/nginx/conf.d/xray.conf <<EOF
+    server {
+             listen 80;
+             listen [::]:80;
+             listen 443 ssl http2 reuseport;
+             listen [::]:443 http2 reuseport;	
+             server_name $domain;
+             ssl_certificate /etc/xray/xray.crt;
+             ssl_certificate_key /etc/xray/xray.key;
+             ssl_ciphers EECDH+CHACHA20:EECDH+CHACHA20-draft:EECDH+ECDSA+AES128:EECDH+aRSA+AES128:RSA+AES128:EECDH+ECDSA+AES256:EECDH+aRSA+AES256:RSA+AES256:EECDH+ECDSA+3DES:EECDH+aRSA+3DES:RSA+3DES:!MD5;
+             ssl_protocols TLSv1.1 TLSv1.2 TLSv1.3;
+             root /home/vps/public_html;
         }
 EOF
-sed -i '$ location ~ / {' /etc/nginx/conf.d/vlnone.conf
-sed -i '$ if ($http_connection = 'Upgrade') {' /etc/nginx/conf.d/vlnone.conf
-sed -i '$ rewrite /(.*) /vlessws break;' /etc/nginx/conf.d/vlnone.conf
-sed -i '$ proxy_pass http://127.0.0.1:14016;' /etc/nginx/conf.d/vlnone.conf
-sed -i '$ proxy_http_version 1.1;' /etc/nginx/conf.d/vlnone.conf
-sed -i '$ proxy_set_header X-Real-IP \$remote_addr;' /etc/nginx/conf.d/vlnone.conf
-sed -i '$ proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;' /etc/nginx/conf.d/vlnone.conf
-sed -i '$ proxy_set_header Upgrade \$http_upgrade;' /etc/nginx/conf.d/vlnone.conf
-sed -i '$ proxy_set_header Connection "upgrade";' /etc/nginx/conf.d/vlnone.conf
-sed -i '$ proxy_set_header Host \$http_host;' /etc/nginx/conf.d/vlnone.conf
-sed -i '$ }' /etc/nginx/conf.d/vlnone.conf
-sed -i '$ }' /etc/nginx/conf.d/vlnone.conf
 
-sed -i '$ ilocation = /vmessws' /etc/nginx/conf.d/xray.conf
+sed -i '$ ilocation = /vmess' /etc/nginx/conf.d/xray.conf
 sed -i '$ i{' /etc/nginx/conf.d/xray.conf
 sed -i '$ iproxy_redirect off;' /etc/nginx/conf.d/xray.conf
 sed -i '$ iproxy_pass http://127.0.0.1:23456;' /etc/nginx/conf.d/xray.conf
@@ -403,10 +397,22 @@ sed -i '$ iproxy_set_header Connection "upgrade";' /etc/nginx/conf.d/xray.conf
 sed -i '$ iproxy_set_header Host \$http_host;' /etc/nginx/conf.d/xray.conf
 sed -i '$ i}' /etc/nginx/conf.d/xray.conf
 
-sed -i '$ ilocation = /trojanws' /etc/nginx/conf.d/xray.conf
+sed -i '$ ilocation = /trojan-ws' /etc/nginx/conf.d/xray.conf
 sed -i '$ i{' /etc/nginx/conf.d/xray.conf
 sed -i '$ iproxy_redirect off;' /etc/nginx/conf.d/xray.conf
 sed -i '$ iproxy_pass http://127.0.0.1:25432;' /etc/nginx/conf.d/xray.conf
+sed -i '$ iproxy_http_version 1.1;' /etc/nginx/conf.d/xray.conf
+sed -i '$ iproxy_set_header X-Real-IP \$remote_addr;' /etc/nginx/conf.d/xray.conf
+sed -i '$ iproxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;' /etc/nginx/conf.d/xray.conf
+sed -i '$ iproxy_set_header Upgrade \$http_upgrade;' /etc/nginx/conf.d/xray.conf
+sed -i '$ iproxy_set_header Connection "upgrade";' /etc/nginx/conf.d/xray.conf
+sed -i '$ iproxy_set_header Host \$http_host;' /etc/nginx/conf.d/xray.conf
+sed -i '$ i}' /etc/nginx/conf.d/xray.conf
+
+sed -i '$ ilocation = /trojango' /etc/nginx/conf.d/xray.conf
+sed -i '$ i{' /etc/nginx/conf.d/xray.conf
+sed -i '$ iproxy_redirect off;' /etc/nginx/conf.d/xray.conf
+sed -i '$ iproxy_pass http://127.0.0.1:2087;' /etc/nginx/conf.d/xray.conf
 sed -i '$ iproxy_http_version 1.1;' /etc/nginx/conf.d/xray.conf
 sed -i '$ iproxy_set_header X-Real-IP \$remote_addr;' /etc/nginx/conf.d/xray.conf
 sed -i '$ iproxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;' /etc/nginx/conf.d/xray.conf
@@ -466,6 +472,26 @@ sed -i '$ igrpc_set_header Host \$http_host;' /etc/nginx/conf.d/xray.conf
 sed -i '$ igrpc_pass grpc://127.0.0.1:33456;' /etc/nginx/conf.d/xray.conf
 sed -i '$ i}' /etc/nginx/conf.d/xray.conf
 
+cat >/etc/nginx/conf.d/vlnone.conf <<EOF
+    server {
+             listen 8880;
+             listen [::]:8880;
+             server_name $domain;
+             real_ip_header proxy_protocol;
+             real_ip_recursive on;
+        }
+EOF
+sed -i '$ ilocation = /vless' /etc/nginx/conf.d/vlnone.conf
+sed -i '$ i{' /etc/nginx/conf.d/vlnone.conf
+sed -i '$ iproxy_redirect off;' /etc/nginx/conf.d/vlnone.conf
+sed -i '$ iproxy_pass http://127.0.0.1:14016;' /etc/nginx/conf.d/vlnone.conf
+sed -i '$ iproxy_http_version 1.1;' /etc/nginx/conf.d/vlnone.conf
+sed -i '$ iproxy_set_header X-Real-IP \$remote_addr;' /etc/nginx/conf.d/vlnone.conf
+sed -i '$ iproxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;' /etc/nginx/conf.d/vlnone.conf
+sed -i '$ iproxy_set_header Upgrade \$http_upgrade;' /etc/nginx/conf.d/vlnone.conf
+sed -i '$ iproxy_set_header Connection "upgrade";' /etc/nginx/conf.d/vlnone.conf
+sed -i '$ iproxy_set_header Host \$http_host;' /etc/nginx/conf.d/vlnone.conf
+sed -i '$ i}' /etc/nginx/conf.d/vlnone.conf
 
 echo -e "$yell[SERVICE]$NC Restart All service"
 systemctl daemon-reload
